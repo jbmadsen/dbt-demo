9ae5e3a7fcd4e753989e82ae0b705bd633aa029f
Jacob B. Madsen <jacob@jbmadsen.com>
2021-03-27 21:42:19 +0100

    Additional config for xp_cmdshell

--- containers/sqlserver/sql/config/config.sql
+++ containers/sqlserver/sql/config/config.sql
@@ -2,3 +2,12 @@ sp_configure 'external scripts enabled', 1;
 RECONFIGURE WITH OVERRIDE; 
 GO
 
+-- this turns on advanced options and is needed to configure xp_cmdshell
+exec sp_configure 'show advanced options', '1';
+RECONFIGURE WITH OVERRIDE; 
+GO
+
+-- this enables xp_cmdshell
+exec sp_configure 'xp_cmdshell', '1';
+RECONFIGURE WITH OVERRIDE; 
+GO
